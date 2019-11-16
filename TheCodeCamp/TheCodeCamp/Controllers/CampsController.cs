e7db6518e2158f3c408a68e69320450c7f71a239
Ganesha Nageshappa <Ganesha.Nageshappa@powerschool.com>
2019-11-16 10:37:51 +0530

    Code camp for .Net and sample project for Code is added

---
+++ TheCodeCamp/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.Data;
+
+namespace TheCodeCamp.Controllers
+{
+    [Serializable]
+    public class CampsController: ApiController
+    {
+        private readonly ICampRepository campRepository;
+
+        public CampsController(ICampRepository campRepository)
+        {
+            this.campRepository = campRepository;
+        }
+        public async Task<IHttpActionResult>  Get()
+        {
+            try
+            {
+                //string s = Url.ToString();
+                //return BadRequest("It wasn't really bad, I made it bad");
+                //return Ok(new { Fname = "A", LName = "B", MName = "C"});
+                var result = await campRepository.GetAllCampsAsync();
+                return Ok(result);  
+            }
+            catch(Exception ex)
+            {
+                return InternalServerError();
+            }
+ 
+        }
+    }
+}
